1447e168b7a2d378a7f9252dd8e2147967a393fe
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-08-16 19:13:22 +0530

    add CORS support and transaction routes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import cors from "cors"
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"}))
+
+// import routes
+
+import transactionRouter from '../src/routes/transaction.routes.js'
+
+// route declaration
+
+app.use("/api/transactions",transactionRouter)
 
 export default app ;
