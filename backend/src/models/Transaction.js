dce631ab600388d4a710213ba681040892c497fc
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-08-16 18:34:11 +0530

    improve the schema by adding default values and additional values

--- backend/src/models/Transaction.js
+++ backend/src/models/Transaction.js
@@ -1,12 +1,39 @@
 import mongoose from 'mongoose';
 
 const transactionSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    category: String,
-    dateofSale: Date,
-    sole: Boolean,
+    title: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['electronics', 'clothings','other']
+    },
+    image: {
+        type: String, // Cloudinary url
+        required: true,
+    },
+    sold: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    dateofSale: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
 });
 
 export const Transaction = mongoose.model('Transaction', transactionSchema);
