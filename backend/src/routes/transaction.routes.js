46d48be915dfa217430e48cff956eda77fc1cae0
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-08-17 13:47:31 +0530

    define routes

---
+++ backend/src/routes/transaction.routes.js
@@ -0,0 +1,17 @@
+import express, { Router } from "express"
+import { listTransactions, 
+         getTransactionStatistics, 
+         getBarChartData, 
+         getPieChartData, 
+         getCombinedData 
+        } from "../controllers/transaction.controller.js"
+
+const router = Router();
+
+router.get('/', listTransactions);
+router.get('/statistics', getTransactionStatistics);
+router.get('/barchart', getBarChartData);
+router.get('/piechart', getPieChartData);
+router.get('/combined', getCombinedData);
+
+export default router 
